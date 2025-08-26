8b05a2e152750e6c0a452c23ff3583dc82815344
Bruce.young19 <Bruce7645@gmail.com>
2025-08-26 17:41:33 +1200

    <FRONTEND> [SKELETON]:   - linked all of the pages to base   - requirements added.   - App.py added all of the skeleton

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template,request,jsonify
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def base():
     return render_template('home.html')
 
 @app.route('/pricing')
@@ -13,6 +13,10 @@ def pricing():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
